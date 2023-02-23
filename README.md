7e5291a487af91b2aff042482486e000263db231
archaudhari <81674825+archaudhari@users.noreply.github.com>
2023-02-23 09:50:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Now-Floats-Assignment---Weather-App
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ejmjqe)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function App() {
+  return (
+    <div>
+      <h5>React Weather App</h5>
+    </div>
+  );
+}
+export default App;
