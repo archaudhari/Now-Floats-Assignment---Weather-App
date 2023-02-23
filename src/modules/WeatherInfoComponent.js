6f0d911a4724d81bedb35f37bdcd6ddb277fa916
archaudhari <81674825+archaudhari@users.noreply.github.com>
2023-02-23 15:26:48 +0530

    1 file modified

--- src/modules/WeatherInfoComponent.js
+++ src/modules/WeatherInfoComponent.js
@@ -27,21 +27,22 @@ margin: 5px auto;
 const Location = styled.span`
 font-size: 28px;
 font-weight: bold;
-`
+`;
 const WeatherInfoLabel = styled.span`
 font-size: 14px;
 font-weight: bold;
 margin: 20px 25px 10px;
 text-align: start;
 width: 90%;
-`
-const WeatherInfoContainer = styled.span`
-font-size: 14px;
-font-weight: bold;
-margin: 20px 25px 10px;
-text-align: start;
+`;
+const WeatherInfoContainer = styled.div`
+display: flex;
 width: 90%;
-`
+flex-direction: row;
+justify-content: space-evenly;
+align-items: center;
+flex-wrap: wrap;
+`;
 
 const InfoContainer = styled.div`
 display: flex;
@@ -49,36 +50,48 @@ margin: 5px 10px;
 flex-direction: row;
 justify-content: space-evenly;
 align-items: center;
-`
-const 
+`;
+const InfoIcon = styled.img`
+width: 36px;
+height: 36px;
+`;
+const InfoLabel = styled.span`
+display: flex;
+flex-direction: column;
+font-size: 14px;
+margin: 15px;
+& span {
+  font-size: 12px;
+  text-transform: capatalize;
+}
+`;
 
 const WeatherInfoComponent = () => {
   return (
     <InfoContainer>
       <InfoIcon />
-      <InfoLabel>
-         
-      </InfoLabel>
+      <InfoLabel></InfoLabel>
     </InfoContainer>
-  )
-}
+  );
+};
 
 const WeatherComponent = () => {
   return (
     <>
       <WeatherCondition>
-        <Condition><span>30℃</span> | Cloudy</Condition>
+        <Condition>
+          <span>30℃</span> | Cloudy
+        </Condition>
         <WeatherLogo src="/icon/perfect-day.svg" />
       </WeatherCondition>
       <Location>London, GB</Location>
       <WeatherInfoLabel>Weather Info</WeatherInfoLabel>
       <WeatherInfoContainer>
-      <WeatherInfoContainer/>
-      <WeatherInfoContainer/>
-      <WeatherInfoContainer/> 
-      <WeatherInfoContainer/> 
+        <WeatherInfoContainer />
+        <WeatherInfoContainer />
+        <WeatherInfoContainer />
+        <WeatherInfoContainer />
       </WeatherInfoContainer>
-
     </>
   );
 };
