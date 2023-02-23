c5254226744af3164cfcefee5c9f82079061b6bd
archaudhari <81674825+archaudhari@users.noreply.github.com>
2023-02-23 18:45:56 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 import CityComponent from './modules/CityComponent';
 import WeatherComponent from './modules/WeatherInfoComponent';
 
+const API_KEY = 'b94ac897d3f376c4535bbff3c38bca1c';
+
 const Container = styled.div`
 display: flex;
 flex-direction: column;
@@ -27,7 +29,12 @@ function App() {
   const [city, updateCity] = useState();
   const [weather, updateWeather] = useState();
 
-  const fetchWeather = () => {};
+  const fetchWeather = async () => {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    );
+    console.response(response);
+  };
   return (
     <Container>
       <AppLabel>React Weather App</AppLabel>
