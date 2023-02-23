9a24242c86455718ca4b99389c79cbb3d30fd90d
archaudhari <81674825+archaudhari@users.noreply.github.com>
2023-02-23 12:06:55 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+margin: auto;
+align-items: center;
+box-shadow: 0 3px 6px 0 #555;
+padding: 20px 10px;
+border-radius: 4px;
+width: 380px;
+background: white;
+font-family: Montserrat;
+`;
+const CityComponent = styled.div`
+display: flex;
+flex-direction: column;
+`;
+const WeatherComponent = styled.div`
+display: flex;
+flex-direction: column;
+`;
+
+const AppLabel = styled.span`
+color: black;
+font-size: 18px;
+font-weight: bold;
+`;
+
 
 function App() {
   return (
-    <div>
-      <h5>React Weather App</h5>
-    </div>
+    <Container>
+      <AppLabel>React Weather App</AppLabel>
+      <CityComponent>CityComponent</CityComponent>
+      <WeatherComponent>WeatherComponent</WeatherComponent>
+    </Container>
   );
 }
 export default App;
